5b0ca0119d3bf7501c984090b580b5eaf32a3750
752878067 <752878067@qq.com>
2017-07-31 15:18:52 +0800

    '计算器'

---
+++ jsq/script/app.js
@@ -0,0 +1,33 @@
+
+/*for(var i=10 ; i > 0 ; i--){//10到1
+
+console.log(i);
+
+}*/
+
+
+/*for(var i=1 ; i<=10; i++){//1到 10
+    console.log(i);
+}*/
+
+
+/*
+var ssc='溜溜你你在在娜娜玉玉兰兰为为哎哎';    //输出偶数字符下标
+ for ( var i = 0 ; i < ssc.length ; i+=2){
+     console.log(ssc[i])
+ }*/
+
+
+/*for( var i=1; i<10 ;i++ ){                           //99乘法口诀
+    for (var j=1; j<10 ; j++ ){
+        console.log(`${i}x${j} = ${i*j}`) ;
+    }
+}*/
+
+/*var n = 1;
+while (n <= 10) {
+  console.log(n);
+  n += 1;
+}
+*/
+
